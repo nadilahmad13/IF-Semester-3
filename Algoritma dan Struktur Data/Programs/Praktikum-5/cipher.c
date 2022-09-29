9dd5d3429156b263435664a02a045a2b30a137a1
IceTeaXXD <16521516@mahasiswa.itb.ac.id>
2022-09-29 10:15:11 +0700

    Updated prak 4

---
+++ Algoritma dan Struktur Data/Programs/Praktikum-5/cipher.c
@@ -0,0 +1,39 @@
+// Nama: Gagas Praharsa Bahar
+// NIM: 13520016
+// Caesar Cipher
+
+#include "boolean.h"
+#include "charmachine.h"
+#include "wordmachine.h"
+// #include "charmachine.c"
+// #include "wordmachine.c"
+#include <stdio.h>
+
+int main(){
+    Word temp;
+    int i;
+    startWord();
+    int geser = currentWord.Length % 26;
+    for(i = 0; i < currentWord.Length; i++){
+        temp.TabWord[i] = currentWord.TabWord[i] + geser;
+        if(temp.TabWord[i] > 90){
+            temp.TabWord[i] -= 26;
+        }
+        printf("%c", temp.TabWord[i]);
+    }
+    advWord();
+    while(!EndWord){
+        printf(" ");
+        int len = currentWord.Length;
+        temp.Length = len;
+        for(i = 0; i < len; i++){
+            temp.TabWord[i] = currentWord.TabWord[i] + geser;
+            if(temp.TabWord[i] > 90){
+                temp.TabWord[i] -= 26;
+            }
+            printf("%c", temp.TabWord[i]);
+        }
+        advWord();
+    }
+    printf(".\n");
+}
