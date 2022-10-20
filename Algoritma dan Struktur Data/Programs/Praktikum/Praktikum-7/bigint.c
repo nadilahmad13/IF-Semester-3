efa039938f0efd1c8adc4ad183c5e1be34d36cd9
Ahmad Nadil <88835532+IceTeaXXD@users.noreply.github.com>
2022-10-20 12:41:28 +0700

    Add files via upload

--- Algoritma dan Struktur Data/Programs/Praktikum/Praktikum-7/bigint.c
+++ Algoritma dan Struktur Data/Programs/Praktikum/Praktikum-7/bigint.c
@@ -1,69 +1,132 @@
 // NIM              : 13521024
 // Nama             : Ahmad Nadil
-// Tanggal          : 19 Oktober 2022
+// Tanggal          : 20 Oktober 2022
 // Topik praktikum  : ADT Stack
 // Deskripsi        : File "bigint.c"
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "stack.c"
+#include "stack.h"
 
-int main(){
-    Stack S1, S2, SPlus;
+boolean IsEqual(Stack S1, Stack S2){
+    Stack temp1, temp2;
+    CreateEmpty(&temp1);
+    CreateEmpty(&temp2);
+
+    temp1 = S1;
+    temp2 = S2;
+
+    int val1,val2;
+    boolean flag = true;
+
+    while (!IsEmpty(temp1) && !IsEmpty(temp2) && flag){
+        Pop(&temp1,&val1);
+        Pop(&temp2,&val2);
+        if (val1 != val2){
+            flag = false;
+        }
+    }
+
+    return flag;
+}
+
+int main()
+{
+    Stack S1, S2, S3;
     CreateEmpty(&S1);
     CreateEmpty(&S2);
-    CreateEmpty(&SPlus);
+    CreateEmpty(&S3);
 
     char tempc;
     int temp;
-    scanf("%c", &tempc);
 
-    while(tempc != '#'){
+    // INPUT S1
+    scanf("%c", &tempc);
+    while(tempc != '\n'){
         temp = tempc - 48;
         Push(&S1, temp);
         scanf("%c", &tempc);
     }
 
+    // INPUT S2
     scanf("%c", &tempc);
-    scanf("%c", &tempc);
-    while(tempc != '#'){
+    while(tempc != '\n'){
         temp = tempc - 48;
         Push(&S2, temp);
         scanf("%c", &tempc);
     }
 
+    // PENGURANGAN
     int carry = 0;
-    while(!IsEmpty(S1) && !IsEmpty(S2)){
+    
+    while(!IsEmpty(S1) && !IsEmpty(S2))
+    {
         Pop(&S1, &temp);
         carry += temp;
-        Pop(&S2, &temp);
-        carry += temp;
-        Push(&SPlus, carry % 10);
-        carry /= 10;
+        Pop(&S2,&temp);
+        carry -= temp;
+        Push(&S3, carry);
+        carry = 0;
     }
-
-    while(!IsEmpty(S1)){
-        Pop(&S1, &temp);
-        carry += temp;
-        Push(&SPlus, carry % 10);
-        carry /= 10;
+    
+    if (ius(S3)){
+        int zero = 0;
+        printf("%d\n",zero);
     }
-
-    while(!IsEmpty(S2)){
-        Pop(&S2, &temp);
-        carry += temp;
-        Push(&SPlus, carry % 10);
-        carry /= 10;
+    else{
+        while(!IsEmpty(S3)){
+            Pop(&S3, &temp);
+            printf("%d", temp);
+        }
     }
-
-    if(carry != 0){
-        Push(&SPlus, carry);
-    }
-
-    while(!IsEmpty(SPlus)){
-        Pop(&SPlus, &temp);
-        printf("%d", temp);
-    }
-
-    printf("\n");
+    return 0;
 }
+
+// int stacktoint (Stack S){
+//     // Stack STemp;
+//     // CreateEmpty(&STemp);
+//     // STemp = S;
+//     int temp;
+//     int result = 0;
+//     int i = 1;
+//     while (!IsEmpty(S)){
+//         result = result + InfoTop(S) * i;
+//         Pop(&S,&temp);
+//         i *= 10;
+//     }
+//     return result;
+// }
+
+// int main()
+// {
+//     Stack S1, S2, S3;
+//     CreateEmpty(&S1);
+//     CreateEmpty(&S2);
+//     CreateEmpty(&S3);
+
+//     char tempc;
+//     int temp;
+
+//     // INPUT S1
+//     scanf("%c", &tempc);
+//     while(tempc != '\n'){
+//         temp = tempc - 48;
+//         Push(&S1, temp);
+//         scanf("%c", &tempc);
+//     }
+
+//     // INPUT S2
+//     scanf("%c", &tempc);
+//     while(tempc != '\n'){
+//         temp = tempc - 48;
+//         Push(&S2, temp);
+//         scanf("%c", &tempc);
+//     }
+
+//     int val1 = stacktoint(S1);
+//     int val2 = stacktoint(S2);
+
+//     printf("%d\n",(val1-val2));
+
+//     return 0;
+// }
