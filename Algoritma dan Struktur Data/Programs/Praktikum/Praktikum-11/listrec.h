344bf6d51872d35746e19528725a2fc22a9877bc
IceTeaXXD <13521024@std.stei.itb.ac.id>
2022-11-24 18:55:06 +0700

    Finished LatPrak 11

--- Algoritma dan Struktur Data/Programs/Praktikum/Praktikum-11/listrec.h
+++ Algoritma dan Struktur Data/Programs/Praktikum/Praktikum-11/listrec.h
@@ -67,4 +67,54 @@ void displayList(List l);
  */
 /* Jika list kosong, tidak menuliskan apa-apa  */
 
+
+/*** Pencarian nilai ekstrim ***/
+/* Prekondisi untuk Max/Min/Sum/Average : List tidak kosong */
+ElType maxList (List l);
+/* Mengirimkan nilai INFO(p) yang maksimum */
+
+ElType minList (List l);
+/* Mengirimkan nilai INFO(p) yang minimum */
+
+ElType sumList (List l);
+/* Mengirimkan total jumlah elemen List l */
+
+float averageList (List l);
+/* Mengirimkan nilai rata-rata elemen list l */
+
+/*** Operasi-Operasi Lain ***/
+List inverseList (List l);
+/* Mengirimkan list baru, hasil invers dari l dengan menyalin semua elemen list.
+Semua elemen list baru harus dialokasi */
+/* Jika alokasi gagal, hasilnya list kosong */
+
+void splitPosNeg (List l, List *l1, List *l2);
+/* I.S. l mungkin kosong */
+/* F.S. Berdasarkan l, dibentuk dua buah list l1 dan l2 */ 
+/* l1 berisi semua elemen l yang positif atau 0, sedangkan l2 berisi
+semua elemen l yang negatif; semua dengan urutan yang sama seperti di l */
+/* l tidak berubah: Semua elemen l1 dan l2 harus dialokasi */
+/* Jika l kosong, maka l1 dan l2 kosong */
+
+void splitOnX (List l, ElType x, List *l1, List *l2);
+/* I.S. l dan x terdefinisi, l1 dan l2 sembarang. */
+/* F.S. l1 berisi semua elemen l yang lebih kecil dari x, dengan urutan
+kemunculan yang sama, l2 berisi semua elemen l yang tidak masuk ke
+l1, dengan urutan kemunculan yang sama. */
+
+int compareList (List l1, List l2);
+/* Menghasilkan: -1 jika l1 < l2, 0 jika l1 = l2, dan 1 jika l1 > l2 */
+/* Jika l[i] adalah elemen l pada urutan ke-i dan |l| adalah panjang l: */
+/* l1 = l2: |l1| = |l2| dan untuk semua i, l1[i] = l2[i] */
+/* l1 < l2: Jika i adalah urutan elemen yang berbeda yang terkecil
+dari l1 dan l2, l1[i]<l2[i] atau: Jika pada semua elemen pada
+urutan i yang sama, l1[i]=l2[i], namun |l1|<|l2| */
+/* Contoh: [3,5,6,7] < [4,4,5,6]; [1,2,3]<[1,2,3,4] */
+/* l1>l2: kebalikan dari l1<l2 */
+
+boolean isAllExist (List l1, List l2);
+/* Menghasilkan true jika semua elemen dalam l1 terdapat dalam l2 (tanpa
+memperhatikan urutan ataupun banyaknya elemen).
+Kedua list mungkin kosong. Jika l1 kosong, maka hasilnya false. */
+
 #endif
--- Algoritma dan Struktur Data/Programs/Praktikum/Praktikum-11/reverse2.c
+++ Algoritma dan Struktur Data/Programs/Praktikum/Praktikum-11/reverse2.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "listrec.h"
+#include "listrec.c"
 
-void displayLinear(List l);
-List inputList (List l, int length);
-List elementsBeforeX(List l, ElType x);
-List InvList (List l);
+// void displayLinear(List l);
+// List inputList (List l, int length);
+// List elementsBeforeX(List l, ElType x);
+// List InvList (List l);
 
 List InvList (List l)
 /* Mengirimkan list baru, hasil invers dari l dengan menyalin semua elemen list.
@@ -58,16 +58,30 @@ int main(){
     List l2 = NULL;
     List l3 = NULL;
 
-    int n,i,temp,index;
+    int n,i,index;
     scanf("%d",&n);
     scanf("%d",&index);
     l1 = inputList(l1,n);
-    l2 = elementsBeforeX(l1,index);
-    l2 = InvList(l2);
-    l1 = InvList(l1);
-    l1 = elementsBeforeX(l1,n-index);
 
-    l3 = concat(l2,l1);
-
-    displayLinear(l3);
+    if (n == 1){
+        printf("%d\n", head(l1));
+    }
+    else if (index <= 1){
+        displayLinear(l1);
+    }
+    else{
+        if (index > n){
+            l1 = InvList(l1);
+            displayLinear(l1);
+        }
+        else{
+            l2 = elementsBeforeX(l1,index);
+            l2 = InvList(l2);
+            l1 = InvList(l1);
+            l1 = elementsBeforeX(l1,n-index);
+            l3 = concat(l2,l1);
+            displayLinear(l3);
+        }
+    }
 }
+// input
